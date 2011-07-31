9539d6ab131454786b6517ab1d62f72cfe6bc292
POYO <mazzonipaul@gmail.com>
2011-07-31 13:08:05 +0200

    Interface web pleinement fonctionnelle ! Changelog : -Possibilité de classer les occurences par auteur, date (en fait l'id, mais ça revient au même) et chan -On affiche plus que les 100 dernières entrées de la bdd. Penser à faire un script de paging pour accèder à toute la base.

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
    <head>
        <title>Oracle - interface de recherche</title>
@@ -6,14 +6,11 @@
 	   <link rel="stylesheet" media="screen" type="text/css" title="Design" href="css.css" />
    </head>
    <body>
-   <center>
-   <h3> Oracle : Interface de recherche</h3>
+   <div id="titre"><h1> Oracle : Interface de recherche</h1></div>
    <div id="corps">
+   <div id="float"><h2> Oracle Recherche, Accepte et Consulte les Liens Etonnants</h2><br/> </div>
+  
 <?php
-try
-{
-
-
 	$base= '~/Oracle/Oracle.sq3';					//Nom du fichier sq3
 	$table = 'oracle';						//Nom de la table
 	// On se connecte à SQLite
@@ -21,60 +18,102 @@ try
     $pdo_options[PDO::ATTR_ERRMODE] = PDO::ERRMODE_EXCEPTION;    
     $bdd = new PDO("sqlite:".$base."");																
     // On récupère tout le contenu de la table  !
-    $reponse = $bdd->query("SELECT * FROM '".$table."' ORDER BY id"); 								
+	
 
-   ?>
+
+
+
    
-   <?php include('recherche.php'); ?>
-	<table>
-   <caption>Index des liens</caption>
+include('recherche.php');
+   
+	echo'<table>
+   <caption>Index des liens<form method="post" action="index.php" id="classement">
+   <span>Classer par :</span>
+<input type="radio" name="classer" value="id" checked="checked"/> <label for="auteur">Date</label>
+<input type="radio" name="classer" value="auteur"/> <label for="auteur">Auteur</label>
+<input type="radio" name="classer" value="chan_orig"/> <label for="auteur">Channel</label>
+<input type="submit" value="Classer" name="classement" />
+</form></caption>
 
    <tr>
-       <th>Pseudo</th>
+       <th>#    </th>
+       <th>Auteur</th>
        <th>Channel</th>
 	   <th>Lien</th>
        <th>Tags</th>
        <th>Heure</th>
-   </tr>
-    <?php 
+   </tr>';
+ 
 
 
-
+$a = 1;
 	// On affiche chaque entrée
-    while ($donnees = $reponse->fetch())
+    	
+	if ($_POST['classement'] == '')
+	{
+	$reponse = $bdd->query("SELECT * FROM '".$table."' ORDER BY id DESC LIMIT 0,100");
+	
+	while ($donnees = $reponse->fetch())
     {
       $date = date('d/m/Y H\hi', $donnees['date']);			//formatage de la date
 $trans = array("," => ", ");											//Mise en place d'espaces entre chaque tags
 
 
-if (strlen($donnees['link']) <= 35)								//Si le lien n'est pas trop long (si il fait moins de x caractères)
+
+if (strlen($donnees['link']) <= 36)								//Si le lien n'est pas trop long (si il fait moins de x caractères)
  { 
- echo  '<tr> <td>'.$donnees['auteur'].'</td><td>'.$donnees['chan_orig'].' </td><td><a href="'.$donnees['link'].'">'.$donnees['link'].'</a></td><td>'.strtr($donnees['keywords'], $trans).'</td><td>'.$date.'</td></tr>';
+
+ echo  '<tr> <td>'.$a++.'   </td><td>'.$donnees['auteur'].'</td><td>'.$donnees['chan_orig'].' </td><td><a href="'.$donnees['link'].'">'.$donnees['link'].'</a></td><td>'.strtr($donnees['keywords'], $trans).'</td><td>'.$date.'</td></tr>';
+}
+else																			//Si le lien est trop long (plus de x caractères)
+{
+
+$fin = substr("".$donnees['link']."", -4); 						//On garde les 4 derniers caractères.
+$debut = substr("".$donnees['link']."", 0, 35);  				//On conserve les 35 premiers
+ echo  '<tr> <td>'.$a++.'   </td><td>'.$donnees['auteur'].'</td><td>'.$donnees['chan_orig'].' </td><td><a href="'.$donnees['link'].'">'.$debut.'...'.$fin.'</a></td><td>'.strtr($donnees['keywords'], $trans).'</td><td>'.$date.'</td></tr>';
+}
+   }
+
+	
+	echo "</table>";
+    
+
+	$reponse->closeCursor(); // Fin du traitement
+
+
+
+}
+else{
+	$classement = $_POST['classer'];
+	$reponse = $bdd->query("SELECT * FROM '".$table."' ORDER BY ".$classement." DESC LIMIT 0,100");
+	while ($donnees = $reponse->fetch())
+    {
+      $date = date('d/m/Y H\hi', $donnees['date']);			//formatage de la date
+$trans = array("," => ", ");											//Mise en place d'espaces entre chaque tags
+
+
+
+if (strlen($donnees['link']) <= 36)								//Si le lien n'est pas trop long (si il fait moins de x caractères)
+ { 
+ echo  '<tr> <td>'.$a++.'   </td><td>'.$donnees['auteur'].'</td><td>'.$donnees['chan_orig'].' </td><td><a href="'.$donnees['link'].'">'.$donnees['link'].'</a></td><td>'.strtr($donnees['keywords'], $trans).'</td><td>'.$date.'</td></tr>';
 }
 else																			//Si le lien est trop long (plus de x caractères)
 {
 $fin = substr("".$donnees['link']."", -4); 						//On garde les 4 derniers caractères.
 $debut = substr("".$donnees['link']."", 0, 35);  				//On conserve les 35 premiers
- echo  '<tr> <td>'.$donnees['auteur'].'</td><td>'.$donnees['chan_orig'].' </td><td><a href="'.$donnees['link'].'">'.$debut.'...'.$fin.'</a></td><td>'.strtr($donnees['keywords'], $trans).'</td><td>'.$date.'</td></tr>';
+ echo  '<tr>  <td>'.$a++.'  </td><td>'.$donnees['auteur'].'</td><td>'.$donnees['chan_orig'].' </td><td><a href="'.$donnees['link'].'">'.$debut.'...'.$fin.'</a></td><td>'.strtr($donnees['keywords'], $trans).'</td><td>'.$date.'</td></tr>';
 }
    }
-    ?>
+
 	
-	</table>
+	echo "</table>";
     
-	<?php
+
 	$reponse->closeCursor(); // Fin du traitement
-
 }
-catch(Exception $e)
-{
-    // En cas d'erreur, on affiche un message
-    die('Erreur : '.$e->getMessage());
-}
+?>
 
-
-?></div><br/>
-<div id="footer"><p>Oracle web interface <a href="http://hgpub.druil.net/Oracle/">http://hgpub.druil.net/Oracle/</a></div>
-</center>
+</div><br/>
+<div id="footer"><p>Oracle web interface <a href="http://hgpub.druil.net/Oracle/">http://hgpub.druil.net/Oracle/</a></p></div>
 </body>
 </html>
