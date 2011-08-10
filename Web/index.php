722f7e468ada6182984c8ea58642156c21a0a4e2
Whidou <whidou@gmail.com>
2011-08-10 02:36:58 +0200

    B

--- Web/index.php
+++ Web/index.php
@@ -16,7 +16,6 @@ if (isset($_POST['tags']) AND isset($_POST['id']))
 	$donnees = $reponse->fetch();
 
 	$bdd->exec("UPDATE ".$table." SET keywords='".$donnees['keywords'].strtr($tags,  array(" " => ",")).",' WHERE id='".$id."'");
-	$bdd->commit();
 } ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
