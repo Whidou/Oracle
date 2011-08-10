44f1460b956029bdcda19c099222a34a98b428a8
Whidou <whidou@gmail.com>
2011-08-10 02:21:51 +0200

    O -> o

--- Web/config.php
+++ Web/config.php
@@ -1,6 +1,6 @@
 <?php
 $base = 'Oracle.sq3';	// Chemin vers la BDD d'Oracle
-$table = '0racle';		// Nom du bot
+$table = 'oracle';		// Nom du bot
 $max_link_length = 36;	// Longueur maximale d'un lien affiché
 $lines = 100;			// Nombre de lignes affichées
 ?>
