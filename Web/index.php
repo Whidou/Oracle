4d7d3770f5d7e1451b2c502365dbe1d0a6aa33a7
Whidou <whidou@gmail.com>
2011-08-11 03:31:26 +0200

    Changement d'organisation.

--- Web/index.php
+++ Web/index.php
@@ -1,3 +1,38 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
+<head>
+	<title>Oracle - interface de recherche</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" media="screen" type="text/css" title="Oracle" href="css.css" />
+</head>
+<body>
+	<div id="corps">
+		<div id="float">
+			<h2> Oracle Recherche, Accepte et Consulte les Liens Etonnants</h2>
+		</div>
+		<form method="get" action="#" id="formulaire">
+			<span>Recherche :</span>
+			<input type="text" name="recherche" />
+			<span>Type :</span>
+			<input type="radio" name="champ" value="keywords" id="r_tags" checked="checked" /><label for="r_tags">Tags</label>
+			<input type="radio" name="champ" value="auteur" id="r_auteur" /> <label for="r_auteur">Auteur</label>
+			<input type="radio" name="champ" value="chan_orig" id="r_chan" /><label for="r_chan">Salon</label>
+			<span>Trier par :</span>
+			<input type="radio" name="sort" value="id" id="date" /> <label for="date">Date</label>
+			<input type="radio" name="sort" value="auteur" id="auteur" /> <label for="auteur">Auteur</label>
+			<input type="radio" name="sort" value="chan_orig" id="chan" /> <label for="chan">Salon</label>
+			<input type="submit" value="Go !" />
+		</form>
+		<table>
+			<tr>
+				<th>#</th>
+				<th>Auteur</th>
+				<th>Salon</th>
+				<th>Lien</th>
+				<th>Tags</th>
+				<th>Heure</th>
+				<th>Modifier les tags</th>
+			</tr>
 <?php
 // Configuration
 include('config.php');
@@ -27,8 +62,6 @@ if (isset($_GET['recherche']))
 }
 else
 {
-	$recherche = "";
-	$champ = "";
 	$search = "";
 }
 
@@ -40,48 +73,8 @@ if (isset($_GET['sort']))
 else
 {
 	$classement = 'id';
-} ?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" >
-<head>
-	<title>Oracle - interface de recherche</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-	<link rel="stylesheet" media="screen" type="text/css" title="Oracle" href="css.css" />
-</head>
-<body>
-	<div id="corps">
-		<div id="float">
-			<h2> Oracle Recherche, Accepte et Consulte les Liens Etonnants</h2>
-		</div>
-		<form method="get" action="#" id="formulaire">
-			<p>Recherche :</p>
-			<input type="text" name="recherche" /><br/>
-			<p>Type :</p>
-			<input type="radio" name="champ" value="keywords" id="r_tags" checked="checked" /><label for="r_tags">Tags</label>
-			<input type="radio" name="champ" value="auteur" id="r_auteur" /> <label for="r_auteur">Auteur</label>
-			<input type="radio" name="champ" value="chan_orig" id="r_chan" /><label for="r_chan">Salon</label>
-			<input type="hidden" name="sort" value="<?echo $classement;?>" />
-		</form>
-		<form method="get" action="#" id="classement">
-			<span>Trier par :</span>
-			<input type="radio" name="sort" value="id" id="date" /> <label for="date">Date</label>
-			<input type="radio" name="sort" value="auteur" id="auteur" /> <label for="auteur">Auteur</label>
-			<input type="radio" name="sort" value="chan_orig" id="chan" /> <label for="chan">Salon</label>
-			<input type="hidden" name="recherche" value="<?echo $recherche;?>" />
-			<input type="hidden" name="champ" value="<?echo $champ;?>" />
-			<input type="submit" value="Trier" />
-		</form>
-		<table>
-			<tr>
-				<th>#</th>
-				<th>Auteur</th>
-				<th>Salon</th>
-				<th>Lien</th>
-				<th>Tags</th>
-				<th>Heure</th>
-				<th>Modifier les tags</th>
-			</tr>
-<?php
+}
+
 //Requête
 $reponse = $bdd->query("SELECT * FROM '".$table."'".$search." ORDER BY ".$classement." DESC LIMIT 0, ".$lines);
 
